12a92dfb930a9d4d08191793f753a568f4c29a63
ephilbin <143443264+ephilbin@users.noreply.github.com>
2023-11-06 14:32:45 -0500

    Add files via upload

---
+++ add.py
@@ -0,0 +1,9 @@
+x = {1,2,3,4,5}
+y = x.add(6)
+print(y)
+#output with just print y is none
+#bc y is the algorithm (process)
+#and you can't print the process
+print(x)
+#prints the actual results 
+#after the algorithm was finished/process has been completed
